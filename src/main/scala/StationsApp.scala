5f6a1b750a673612a4e25ee927db529de16e93df
Erik Torres <erik.torres@lifullconnect.com>
2024-02-05 15:01:04 +0100

    :bug: Update ref

--- src/main/scala/StationsApp.scala
+++ src/main/scala/StationsApp.scala
@@ -8,14 +8,10 @@ import fs2.io.file.Path
 
 object StationsApp extends IOApp:
   override def run(args: List[String]): IO[ExitCode] = args match
-    case measurementsPath :: maxRows :: parallelism :: Nil =>
+    case measurementsPath :: Nil =>
       for
         _ <- IO.println(s"Reading measurements from: $measurementsPath")
-        stats <- MeasurementsAnalyser.analyse(
-          Path(measurementsPath),
-          maxRows.toInt,
-          parallelism.toInt,
-        )
+        stats <- MeasurementsAnalyser.analyse(Path(measurementsPath))
         _ <- stats.toList.sortBy { case (stationName, _) => stationName }.traverse {
           case (stationName, stats) =>
             IO.println(s"$stationName=${stats.min}/${stats.mean}/${stats.max}")
--- src/main/scala/domain/MeasurementsAnalyser.scala
+++ src/main/scala/domain/MeasurementsAnalyser.scala
@@ -2,72 +2,38 @@ package es.eriktorr.weather
 package domain
 
 import cats.effect.{IO, Ref}
-import cats.implicits.toTraverseOps
 import fs2.io.file.{Files, Path}
-import fs2.Stream
 
 object MeasurementsAnalyser:
-  def analyse(
-      measurementsPath: Path,
-      maxRows: Int = 1_000_000_000,
-      parallelism: Int = 2,
-  ): IO[Map[String, Stats]] =
+  def analyse(measurementsPath: Path): IO[Map[String, Stats]] =
     for
-      processorToStatsRef <- Range(0, parallelism).toList
-        .traverse(processor =>
-          Ref.of[IO, Map[String, Stats]](Map.empty[String, Stats]).map(processor -> _),
-        )
-        .map(_.toMap)
-      _ <- Stream
-        .range(0, parallelism)
-        .map(processor =>
-          Files[IO]
-            .readUtf8Lines(measurementsPath)
-            .take(maxRows)
-            .filter(_.charAt(0).toInt % parallelism == processor)
-            .evalMap(updateStatsWith(_, processorToStatsRef(processor))),
-        )
-        .parJoin(parallelism)
+      statsRef <- Ref.of[IO, Map[String, Stats]](Map.empty[String, Stats])
+      _ <- Files[IO]
+        .readUtf8Lines(measurementsPath)
+        .filter(_.trim.nn.nonEmpty)
+        .evalMap(updateStatsWith(_, statsRef))
         .compile
         .toList
-      statsByProcessor <- processorToStatsRef.values.toList.traverse(_.get)
-      allStats = statsByProcessor.fold(Map.empty) { case (x, y) =>
-        x ++ y.toList.map { case (stationName, stats) =>
-          x.get(stationName) match
-            case Some(other) =>
-              val updatedStats =
-                Stats(
-                  count = stats.count + other.count,
-                  min = Math.min(stats.min, other.min),
-                  max = Math.max(stats.max, other.max),
-                  sum = stats.sum + other.sum,
-                )
-              stationName -> updatedStats
-            case None => stationName -> stats
-        }.toMap
-      }
-    yield allStats
+      stats <- statsRef.get
+    yield stats
 
   private def updateStatsWith(line: String, statsRef: Ref[IO, Map[String, Stats]]) =
     import scala.language.unsafeNulls
     line.split(";", 2).toList match
       case stationName :: measurement :: Nil =>
         val temperature = measurement.toDouble
-        for
-          stationNameToStats <- statsRef.get
-          currentStats = stationNameToStats.getOrElse(
+        statsRef.update { stationNameToStats =>
+          val currentStats = stationNameToStats.getOrElse(
             stationName,
             Stats(0L, temperature, temperature, 0.0d),
           )
-          _ <- statsRef.update(
-            _ + (stationName -> Stats(
-              count = currentStats.count + 1L,
-              min = Math.min(currentStats.min, temperature),
-              max = Math.max(currentStats.max, temperature),
-              sum = currentStats.sum + temperature,
-            )),
-          )
-        yield ()
+          stationNameToStats + (stationName -> Stats(
+            count = currentStats.count + 1L,
+            min = Math.min(currentStats.min, temperature),
+            max = Math.max(currentStats.max, temperature),
+            sum = currentStats.sum + temperature,
+          ))
+        }
       case _ =>
         IO.raiseError(
           IllegalArgumentException(
